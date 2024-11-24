845bc2466c6416c9d62028524893b760808cc427
Đăng Việt <trandangviet00@gmail.com>
2024-11-24 22:22:54 +0700

    Add files via upload

--- ss4-b8.c
+++ ss4-b8.c
@@ -2,8 +2,9 @@
 
 int main(){
 	int a,b,c;
+	
 	printf("Moi nhap 3 canh cua tam giac:");
-     scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c);
 
     if (a+b>c && a+c>b && b+c>a){
 	printf("La 3 canh tam giac");
