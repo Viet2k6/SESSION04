5ef5aa60314622da05a0e00dd78ce95f5a83ea5f
Đăng Việt <trandangviet00@gmail.com>
2024-11-24 22:20:38 +0700

    Add files via upload

--- ss4-b8.c
+++ ss4-b8.c
@@ -2,13 +2,12 @@
 
 int main(){
 	int a,b,c;
-	
-printf("Moi nhap 3 canh cua tam giac:");
-scanf("%d %d %d", &a, &b, &c);
+	printf("Moi nhap 3 canh cua tam giac:");
+    scanf("%d %d %d", &a, &b, &c);
 
-if (a+b>c && a+c>b && b+c>a){
+    if (a+b>c && a+c>b && b+c>a){
 	printf("La 3 canh tam giac");
-}else{printf("Khong phai 3 canh tam giac");
+    }else{printf("Khong phai 3 canh tam giac");
 }
 
    return 0;
